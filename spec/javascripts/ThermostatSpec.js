0e9a690d0029060bee52462ab6907856926a77f0
dtrts <djt210@gmail.com>
2019-08-21 12:35:36 +0100

    RED up and down functions and minimum temp value.

--- spec/javascripts/ThermostatSpec.js
+++ spec/javascripts/ThermostatSpec.js
@@ -9,4 +9,29 @@ describe('Thermostat', function () {
   it('starts at 20', function () {
     expect(thermo.temp).toEqual(20);
   });
+
+  it('increases temp with up function', function () {
+    increase_by = Math.floor(Math.random() * 100) + 1
+    thermo.up(increase_by)
+    expect(thermo.temp).toEqual(20 + increase_by)
+  });
+  it('increases temp with down function', function () {
+    decrease_by = Math.floor(Math.random() * 10) - 1
+    thermo.up(decrease_by)
+    expect(thermo.temp).toEqual(20 + decrease_by)
+  });
+
+  it('cannot decrease temp below 10deg', function () {
+    Expect(function () {
+      thermo.down(11)
+    }).toThrowError('Unable to set temperature.')
+  });
+
 });
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
