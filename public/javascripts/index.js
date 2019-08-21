692600cf476734f66db4f91d731a888d6558eb16
dtrts <djt210@gmail.com>
2019-08-21 17:02:45 +0100

    REFACTOR

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,56 +1,78 @@
 $(document).ready(function () {
+
+  var message_timeout;
   var thermo = new Thermostat();
-  $("#temp-display").text(thermo.temp);
-  var t;
 
-  $("#up-1-button").click(function () {
-    clearInterval(t);
-    try {
-      thermo.up(1);
-      $("#message-display").text("You clicked the 1up button");
-      $("#temp-display").text(thermo.temp);
-    } catch (e) {
-      $("#message-display").text(e);
-    }
+  console.log(thermo);
+  console.log(thermo.constructor);
+  console.log(this);
+  console.log(this.constructor);
+  console.log(this.__proto__);
+  console.log("where the fek am i");
 
-    t = setTimeout(function () {
-      $("#message-display").text("Thermostat!")
+  function set_default_message() {
+    $("#message-display").text("Thermostat!");
+  };
+
+  function default_message_timeout() {
+    return setTimeout(function () {
+      set_default_message();
     }, 2500)
-  });
-  $("#down-1-button").click(function () {
-    clearInterval(t);
+  }
+
+  function display_current_temp() {
+    $("#temp-display").text(thermo.temp);
+  }
+
+  function click_down_1() {
+    clearInterval(message_timeout);
     try {
       thermo.down(1);
       $("#message-display").text("You clicked the 1down button");
-      $("#temp-display").text(thermo.temp);
+      display_current_temp();
     } catch (e) {
       $("#message-display").text(e);
     }
 
-    t = setTimeout(function () {
-      $("#message-display").text("Thermostat!")
-    }, 2500)
+    message_timeout = default_message_timeout()
+  };
+
+
+  display_current_temp();
+
+  $("#up-1-button").click(function () {
+    console.log("Upping 1 nasfhafniuakjnkjsd")
+    console.log(this)
+    clearInterval(message_timeout);
+    try {
+      thermo.up(1);
+      $("#message-display").text("You clicked the 1up button");
+      display_current_temp();
+    } catch (e) {
+      $("#message-display").text(e);
+    }
+
+    message_timeout = default_message_timeout()
   });
+  $("#down-1-button").click(click_down_1());
+
   $("#reset-button").click(function () {
-    clearInterval(t);
+    clearInterval(message_timeout);
     thermo.reset()
     $("#message-display").text("You clicked the reset button");
-    $("#temp-display").text(thermo.temp);
+    display_current_temp();
 
-    t = setTimeout(function () {
-      $("#message-display").text("Thermostat!")
-    }, 2500)
+    message_timeout = default_message_timeout()
   });
   $("#switch-power-saving-button").click(function () {
-    clearInterval(t);
+    clearInterval(message_timeout);
     thermo.switch_power_saving()
     $("#message-display").text("You clicked the switch power saving button");
     $("#power-saving-mode-display").text(`POWER SAVING IS ${thermo.power_saving ? "ON" : "OFF"}.`);
-    $("#temp-display").text(thermo.temp);
+    display_current_temp();
 
-    t = setTimeout(function () {
-      $("#message-display").text("Thermostat!")
-    }, 2500)
+    message_timeout = default_message_timeout()
   });
+
 
 });
