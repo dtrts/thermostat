fc39d7d642c2126a195d6de4e5de1565efa74aaf
dtrts <djt210@gmail.com>
2019-08-21 12:40:58 +0100

    REFACTOR making sure to use strict across scripts.

--- public/javascripts/Thermostat.js
+++ public/javascripts/Thermostat.js
@@ -1,15 +1,19 @@
+'use strict';
+
 var Thermostat = function () {
   this.temp = 20;
+  this.min_temp = 10;
 };
 
 Thermostat.prototype = {
   up: function (increase_by) {
-    this.temp += increase_by
+    this.temp += increase_by;
   },
   down: function (decrease_by) {
     if (this.temp -= decrease_by < this.min_temp) {
-      throw new Error('Unable to set temperature.')
+      throw new Error('Unable to set temperature.');
     }
-    this.temp -= decrease_by
+    this.temp -= decrease_by;
   },
+
 };
--- spec/javascripts/ThermostatSpec.js
+++ spec/javascripts/ThermostatSpec.js
@@ -1,4 +1,4 @@
-'user strict';
+'use strict';
 
 describe('Thermostat', function () {
   var thermo;
@@ -11,27 +11,21 @@ describe('Thermostat', function () {
   });
 
   it('increases temp with up function', function () {
-    increase_by = Math.floor(Math.random() * 100) + 1
+    var increase_by = Math.floor(Math.random() * 100) + 1
     thermo.up(increase_by)
     expect(thermo.temp).toEqual(20 + increase_by)
   });
   it('increases temp with down function', function () {
-    decrease_by = Math.floor(Math.random() * 10) - 1
+    var decrease_by = Math.floor(Math.random() * 10) - 1
     thermo.up(decrease_by)
     expect(thermo.temp).toEqual(20 + decrease_by)
   });
 
   it('cannot decrease temp below 10deg', function () {
+    expect(thermo.min_temp).toEqual(10)
     expect(function () {
       thermo.down(11)
     }).toThrowError('Unable to set temperature.')
   });
 
 });
-
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
